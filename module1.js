00e042680f08c10710e4f16c77993adf3f6ec9a5
aayush baluni <sportyayushbaluni993@gmail.com>
2023-01-03 20:43:06 +0530

    Learning

---
+++ module1.js
@@ -0,0 +1,5 @@
+function log(message) {
+    console.log(message);
+}
+// what we want to call it outside = inside  var to export;
+module.exports.log = log;
--- node-basics.js
+++ node-basics.js
@@ -1,1 +1,11 @@
+var logger=require('./module1');
+
 console.log("Hello");
+
+var hey = function () {
+    console.log("Hey Function.");
+    return;
+}
+
+logger.log(hey());
+logger.log("HELLO");
