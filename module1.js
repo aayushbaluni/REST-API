878fcdb6b267ea8bb21c851b62b47b067e8e8d86
aayush baluni <sportyayushbaluni993@gmail.com>
2023-01-03 20:46:35 +0530

    Learning

--- module1.js
+++ module1.js
@@ -3,3 +3,6 @@ function log(message) {
 }
 // what we want to call it outside = inside  var to export;
 module.exports.log = log;
+//to export only the function we can use
+//      module.exports=log
+// and the require object is directly an object then.
