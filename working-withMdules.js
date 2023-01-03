4430194ad05d58217d6af7c1fefe9cefc855d0c0
aayush baluni <sportyayushbaluni993@gmail.com>
2023-01-03 21:12:12 +0530

    Learning

---
+++ working-withMdules.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const os = require('os');
+const file = require('fs');
+var obj = path.parse(__filename);
+console.log(obj);
+
+var a = os.totalmem();
+var b = os.freemem();
+console.log({ a, b });
+
+//Syncronously
+var files = file.readdirSync('./');
+console.log(files);
+
+//non-Syncronous
+
+var f = file.readdir('./', ((err, fil) => {
+    if (err) console.log(err);
+    else console.log(fil);
+}));
