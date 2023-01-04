919e9126485f9e3be0523fdae53e499c40a8f82b
aayush baluni <sportyayushbaluni993@gmail.com>
2023-01-04 21:32:35 +0530

    Learning

--- npm-demo/index.js
+++ npm-demo/index.js
@@ -1,3 +1,14 @@
 var _ = require('underscore');
 
 console.log(_.contains([1, 2, 3], 3));
+
+
+// Major.Minor.patch
+
+// also use ~ in place of ^ which means patches up to latest but major and moinor remains same.
+// ~1.1.2
+
+//to install specific version 
+//npm i name@version
+//npm un package -> to uninstall
+//to check for updates install npm-check-updates
